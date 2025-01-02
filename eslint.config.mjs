3a20ec88d97a3ec035b8c50a1b665ab96ecd57db
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-01-02 16:51:48 -0500

    add eslint, prettier, and lint workflows

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,44 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
 import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/stylistic",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+  ),
+  {
+    files: ["**/*.{js,jsx,ts,tsx,mdx,mjs}"],
+    plugins: {
+      prettier,
+    },
+    languageOptions: {
+      globals: globals.browser,
+      parser: tsParser,
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { caughtErrors: "none" }],
+      "prefer-const": ["error", { destructuring: "all" }],
+      "prettier/prettier": "warn",
+    },
+  },
 ];
 
-export default eslintConfig;
+export default config;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,35 @@
 import type { AppProps } from "next/app";
 import "./global.css";
+import Head from "next/head";
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+/** site variables */
+const {
+  NEXT_PUBLIC_TITLE: TITLE,
+  NEXT_PUBLIC_DESCRIPTION: DESCRIPTION,
+  NEXT_PUBLIC_URL: URL,
+} = process.env;
+
+/** root page layout */
+const App = ({ Component, pageProps }: AppProps) => {
+  /** page title + site title */
+  const fullTitle = [pageProps.title, TITLE].filter(Boolean).join(" | ");
+
+  return (
+    <>
+      <Head>
+        <title>{fullTitle}</title>
+
+        <meta name="title" content={fullTitle} />
+        <meta name="description" content={DESCRIPTION} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={URL} />
+        <meta property="og:title" content={fullTitle} />
+        <meta property="og:description" content={DESCRIPTION} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default App;
